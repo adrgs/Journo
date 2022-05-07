108508255d9311e6b42b08289c061b4727d7a971
Dragos Albastroiu <albastro@adobe.com>
2022-05-07 22:05:16 +0300

    Implement Firebase register code

--- Journo/Resources/FirebaseDatabaseManager.swift
+++ Journo/Resources/FirebaseDatabaseManager.swift
@@ -6,8 +6,26 @@
 //
 
 import Foundation
+import FirebaseDatabase
 
 public class FirebaseDatabaseManager {
     static let shared = FirebaseDatabaseManager()
     
+    private let database = Database.database().reference()
+    
+    public func canCreateNewUser(email: String, username: String, completion: (Bool) -> Void) {
+        completion(true)
+    }
+    
+    public func insertNewUser(email: String, username: String, completion: @escaping (Bool) -> Void) {
+        database.child(email.safeDatabaseKey()).setValue(["username": username], withCompletionBlock: { error, _ in
+            if error == nil {
+                completion(true)
+                return
+            } else {
+                completion(false)
+                return
+            }
+        })
+    }
 }
