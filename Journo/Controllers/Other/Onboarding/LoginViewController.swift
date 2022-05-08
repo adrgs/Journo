57f3022e0048499d63508cb3d42f7927f1e37720
Dragos Albastroiu <albastro@adobe.com>
2022-05-08 08:34:42 +0300

    Implement Google Authentication

--- Journo/Controllers/Other/Onboarding/LoginViewController.swift
+++ Journo/Controllers/Other/Onboarding/LoginViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SafariServices
 import FirebaseAuth
+import GoogleSignIn
 
 class LoginViewController: UIViewController {
     
@@ -18,7 +19,7 @@ class LoginViewController: UIViewController {
     private let usernameEmailField: UITextField = {
         let field = UITextField()
         
-        field.placeholder = "Username or Email"
+        field.placeholder = "Email"
         field.returnKeyType = .next
         field.leftViewMode = .always
         field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
@@ -73,6 +74,8 @@ class LoginViewController: UIViewController {
         return button
     }()
     
+    private let googleButton = GIDSignInButton()
+    
     private let termsButton: UIButton = {
         let button = UIButton()
         
@@ -109,6 +112,7 @@ class LoginViewController: UIViewController {
         createAccountButton.addTarget(self, action: #selector(didClickCreateAccountButton), for: .touchUpInside)
         privacyButton.addTarget(self, action: #selector(didClickPrivacyButton), for: .touchUpInside)
         termsButton.addTarget(self, action: #selector(didClickTermsButton), for: .touchUpInside)
+        googleButton.addTarget(self, action: #selector(didClickGoogleButton), for: .touchUpInside)
 
         usernameEmailField.delegate = self
         passwordField.delegate = self
@@ -128,6 +132,7 @@ class LoginViewController: UIViewController {
         passwordField.frame = CGRect(x: 25, y: usernameEmailField.bottom + 10, width: view.width-50, height: 52)
         loginButton.frame = CGRect(x: 25, y: passwordField.bottom + 10, width: view.width-50, height: 52)
         createAccountButton.frame = CGRect(x: 25, y: loginButton.bottom + 10, width: view.width-50, height: 52)
+        googleButton.frame = CGRect(x: 25, y: createAccountButton.bottom + 10, width: view.width-50, height: 52)
         
         termsButton.frame = CGRect(x: 10, y: view.height - view.safeAreaInsets.bottom - 100, width: view.width-20, height: 50)
         privacyButton.frame = CGRect(x: 10, y: view.height - view.safeAreaInsets.bottom - 50, width: view.width-20, height: 50)
@@ -157,6 +162,7 @@ class LoginViewController: UIViewController {
         view.addSubview(passwordField)
         view.addSubview(loginButton)
         view.addSubview(createAccountButton)
+        view.addSubview(googleButton)
         view.addSubview(termsButton)
         view.addSubview(privacyButton)
         view.addSubview(headerView)
@@ -219,6 +225,53 @@ class LoginViewController: UIViewController {
         }
         let vc = SFSafariViewController(url:url)
         present(vc, animated: true)
+    }
+    
+    @objc private func didClickGoogleButton() {
+        let signInConfig = GIDConfiguration.init(clientID: "997115990327-2qtqs1d7nqak6s3mhpm7t0urktetimh9.apps.googleusercontent.com")
+        
+        GIDSignIn.sharedInstance.signIn(with: signInConfig, presenting: self) { user, error in
+            guard error == nil else { return }
+            guard let user = user else { return }
+
+            if let profiledata = user.profile {
+                
+                let userId : String = user.userID ?? ""
+                let givenName : String = profiledata.givenName ?? ""
+                let familyName : String = profiledata.familyName ?? ""
+                let email : String = profiledata.email
+                
+                if let imgurl = user.profile?.imageURL(withDimension: 100) {
+                    let absoluteurl : String = imgurl.absoluteString
+                    //HERE CALL YOUR SERVER API
+                    print(absoluteurl)
+                }
+                print(userId)
+                print(givenName)
+                print(familyName)
+                print(email)
+                
+                let authentication = user.authentication
+                guard let idToken = authentication.idToken else {
+                    return
+                }
+                
+                let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: authentication.accessToken)
+                
+                Auth.auth().signIn(with: credential) { [unowned self] (_, error) in
+                  if error != nil {
+                      let alert = UIAlertController(title: "Google Log In Error", message: "Unable to log in with Google", preferredStyle: .alert)
+                      
+                      alert.addAction(UIAlertAction(title:"Dismiss", style: .cancel, handler: nil))
+                      
+                      self.present(alert, animated: true)
+                  } else {
+                      self.dismiss(animated:true, completion: nil)
+                  }
+                }
+                
+            }
+        }
     }
 
 }
